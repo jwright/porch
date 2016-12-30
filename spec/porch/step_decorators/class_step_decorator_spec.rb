eaad0f85c17ce6927d94172da638b342bd4c6246
Jamie Wright <jamie@brilliantfantastic.com>
2016-12-30 13:44:46 -0500

    Implement the ClassStepDecorator execute method

--- spec/porch/step_decorators/class_step_decorator_spec.rb
+++ spec/porch/step_decorators/class_step_decorator_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Porch::ClassStepDecorator do
       expect(described_class).to_not be_decorates Object.new
     end
   end
+
+  describe "#execute" do
+    class DummyClass
+      def call(context)
+      end
+    end
+
+    let(:context) { Hash.new }
+    let(:step) { DummyClass }
+    subject { described_class.new step }
+
+    it "instantiates an instance of the object and sends call message with the context" do
+      expect_any_instance_of(step).to receive(:call).with context
+
+      subject.execute context
+    end
+  end
 end
