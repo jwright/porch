7805bc897f65df0588060711be04ac5af24413e3
Jamie Wright <jamie@brilliantfantastic.com>
2016-12-29 23:15:22 -0500

    Step collection implementation within the step chain

--- lib/porch/step_chain.rb
+++ lib/porch/step_chain.rb
@@ -1,5 +1,23 @@
 module Porch
   class StepChain
+    attr_reader :steps
+
+    def initialize
+      @steps = []
+    end
+
+    def add(step)
+      steps << step
+    end
+
+    def insert(index, step)
+      steps.insert index, step
+    end
+
+    def remove(step)
+      @steps.delete step
+    end
+
     def execute(context)
       context
     end
---
+++ spec/porch/step_chain_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe Porch::StepChain do
+  it "initializes with empty steps" do
+    expect(subject.steps).to be_empty
+  end
+
+  describe "#add" do
+    it "adds a step to the chain" do
+      expect { subject.add :blah }.to change { subject.steps.count }.by 1
+    end
+  end
+
+  describe "#insert" do
+    it "adds a step to the specific index in the chain" do
+      subject.add :bleh
+
+      expect { subject.insert 0, :blah }.to change { subject.steps.count }.by 1
+      expect(subject.steps.first).to eq :blah
+    end
+  end
+
+  describe "#remove" do
+    it "removes the step from the chain" do
+      subject.add :blah
+      subject.add :bleh
+
+      expect { subject.remove :blah }.to change { subject.steps.count }.by -1
+      expect(subject.steps).to_not include :blah
+      expect(subject.steps).to include :bleh
+    end
+  end
+end
