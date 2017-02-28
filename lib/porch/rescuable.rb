5d55ae32a0778fb47bdf89715d5e78ee2bdc17b7
Jamie Wright <jamie@brilliantfantastic.com>
2017-02-27 20:56:41 -0500

    Find the exception and call the rescue block

--- lib/porch/rescuable.rb
+++ lib/porch/rescuable.rb
@@ -4,8 +4,36 @@ module Porch
       base.extend ClassMethods
     end
 
+    def handler_for_rescue(exception)
+      _, handler = self.class.rescue_handlers.detect do |exception_class, _|
+        exception_class === exception
+      end
+
+      handler
+    end
+
+    def rescue_with_handler(exception)
+      handler = handler_for_rescue(exception)
+      unless handler.nil?
+        self.instance_exec exception, &handler
+        exception
+      end
+    end
+
     module ClassMethods
       def rescue_from(*klasses, with: nil, &block)
+        handler = block_given? ? block : with
+        raise ArgumentError, \
+          "Requires a handler. Use the with keyword argument or supply a block." \
+          if handler.nil?
+
+        klasses.each do |klass|
+          rescue_handlers << [klass, handler]
+        end
+      end
+
+      def rescue_handlers
+        @rescue_handlers ||= []
       end
     end
   end
--- spec/porch/rescuable_spec.rb
+++ spec/porch/rescuable_spec.rb
@@ -1,8 +1,47 @@
 RSpec.describe Porch::Rescuable do
-  let(:dummy_class) { Class.new { include Porch::Rescuable } }
-  subject { dummy_class.new }
+  class WeirdError < RuntimeError; end
+
+  class ReallyScrewedUpClass
+    include Porch::Rescuable
+
+    attr_reader :result
+
+    rescue_from WeirdError do |exception|
+      @result = exception
+    end
+
+    def safely_raise_error(error)
+      raise_error error
+    rescue Exception => e
+      rescue_with_handler(e) || raise(e)
+    end
+
+    def raise_error(error)
+      raise error
+    end
+  end
+
+  subject { ReallyScrewedUpClass.new }
 
   it "adds a `rescue_from` class method" do
     expect(subject.class).to respond_to(:rescue_from)
+  end
+
+  it "handles the error" do
+    expect { subject.safely_raise_error WeirdError }.to_not raise_error
+  end
+
+  it "raises an exception if a method name or proc are not provided" do
+    expect { subject.class.rescue_from RuntimeError }.to raise_error ArgumentError
+  end
+
+  it "is called when the exception class matches" do
+    subject.safely_raise_error WeirdError
+
+    expect(subject.result).to be_kind_of WeirdError
+  end
+
+  it "is not called when the exception does not match" do
+    expect { subject.safely_raise_error RuntimeError }.to raise_error RuntimeError
   end
 end
